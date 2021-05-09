dac4c799294d1476aaf40b629723f99807ab9ce0
Levin Meir <levinmm@gmail.com>
2021-05-09 18:02:54 +0300

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { SiteLayoutComponent } from './components/site-layout/site-layout.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
-];
+  {path:'', component: AuthLayoutComponent, children:
+    [{path:'', redirectTo: 'register', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+   {path: 'register', component: RegisterPageComponent}]
+  }];
+  // {path: '', component: SiteLayoutComponent, children:[]}
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './components/site-layout/site-layout.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
